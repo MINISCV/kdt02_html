45671fbe949a62a5785c7f424fa28c0bc26e46d7
miniscv <ahdzl126@naver.com>
2025-06-02 15:35:50 +0900

    js연습

--- 11/11.js
+++ 11/11.js
@@ -8,33 +8,41 @@ const getDate = () => {
 }
 
 const getMovieData = (date, ul) => {
-    const url = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=6a297044a9c708817d3e785f3308ca25&targetDt=${date.value.replaceAll("-", "")}&itemPerPage=10`;
-        fetch(url)
-            .then(resp => resp.json())
-            .then(data => {
-                const dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
-                const mvList = dailyBoxOfficeList.map((item) => `<li>
-            <span class="spRank">${item.rank}</span> 
-            <span class="spMv">${item.movieNm}</span>
-            <span class="spInten">${item.rankInten == "0" ? "" : Math.abs(parseInt(item.rankInten))}
-             ${parseInt(item.rankInten) > 0 ? '<i class="fa-solid fa-arrow-up"></i>'
-                        : parseInt(item.rankInten) < 0 ? '<i class="fa-solid fa-arrow-down"></i>'
-                            : '<i class="fa-solid fa-minus"></i>'}</span>
-            
-            </li>`);
-                ul.innerHTML = mvList.join('');
-                console.log(dailyBoxOfficeList)
-            })
-            .catch(err => console.log(err));
+    const apiKey = "";
+    let url = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${apiKey}&targetDt=${date.value.replaceAll("-", "")}&itemPerPage=10`;
+    const movieType = document.querySelector("input[type=radio]:checked");
+    console.log(movieType.id);
+    if (movieType.id === 'commercial') {
+        url += "&multiMovieYn=N";
+    } else if (movieType.id === 'multi') {
+        url += "&multiMovieYn=Y";
+    }
+    fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            const dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
+            const mvList = dailyBoxOfficeList.map((item) => `<li>
+        <span class="spRank">${item.rank}</span> 
+        <span class="spMv">${item.movieNm}</span>
+        <span class="spInten">${item.rankInten == "0" ? "" : Math.abs(parseInt(item.rankInten))}
+            ${parseInt(item.rankInten) > 0 ? '<i class="fa-solid fa-arrow-up"></i>'
+                    : parseInt(item.rankInten) < 0 ? '<i class="fa-solid fa-arrow-down"></i>'
+                        : '<i class="fa-solid fa-minus"></i>'}</span>
+        
+        </li>`);
+            ul.innerHTML = mvList.join('');
+        })
+        .catch(err => console.log(err));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const date = document.querySelector("input");
     const ul = document.querySelector("main > ul");
+    const movieType = document.querySelectorAll("input[type=radio]");
+
     date.value = getDate();
     date.setAttribute("max", getDate());
     getMovieData(date, ul);
     date.addEventListener("input", () => getMovieData(date, ul));
+    movieType.forEach(r => r.addEventListener("change", () => getMovieData(date, ul)));
 });
-
-{/* <span class="spNew">${item.rankOldAndNew == "NEW" ? "NEW" : ""}</span> */ }
